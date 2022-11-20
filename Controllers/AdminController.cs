36cc75c4da222dcee8cde4ddee2b48587e65f195
Huseyin Ceylan <huseyinceylantr@gmail.com>
2022-11-20 09:03:19 +0300

    web

---
+++ Controllers/AdminController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace KUSYS.Web.Controllers
+{
+	public class AdminController : BaseController
+	{
+		public IActionResult Home()
+		{
+			return RedirectToAction("Students","Student");
+		}
+
+		
+	}
+}
